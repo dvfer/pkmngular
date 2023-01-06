de246d3dec3faabdfebb2c57d69d3e181844be72
dvfer <diego.pablo23@gmail.com>
2023-01-06 09:55:08 -0300

    update: functions to async await

--- src/app/Services/auth-service.service.ts
+++ src/app/Services/auth-service.service.ts
@@ -3,11 +3,10 @@ import { GoogleAuthProvider, UserInfo } from "firebase/auth";
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Observable } from 'rxjs';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthServiceService {
   constructor( public auth: AngularFireAuth ) {
     this.getObservableCurrentUser.subscribe(( value: UserInfo | null) => {
@@ -19,32 +18,17 @@ export class AuthServiceService {
  * Returns PREGUNTAR QUE PONER
  * @returns
  */
-  logIn(){
-    return this.auth
-      .signInWithPopup(new GoogleAuthProvider())
-      .then((result) => {
-        console.log('You have been successfully logged in!');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  async logIn(){
+    return await this.auth.signInWithPopup(new GoogleAuthProvider())
   }
 
-  logOut(){
-    return this.auth.signOut()
-      .then( (result) =>  {
-        console.log('ok, logged out ');
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+  async logOut(){
+    return await this.auth.signOut()
   }
 /**
  * Returns PREGUNTAR PQ LO UNICO QUE SE ME OCURRE ES REDUNDANTE CON LO DE ABAJO.
  *
- *
  * @returns An observable with the user info or null
- *
  *
  */
   public get getObservableCurrentUser():Observable < UserInfo | null > {
